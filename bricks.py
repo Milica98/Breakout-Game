e71615859515c566b62755c9f0a4e3c5e713f0f4
Milica <55503356+Milica98@users.noreply.github.com>
2024-01-21 00:19:53 +0100

    Revert "Add score" (#3)

--- bricks.py
+++ bricks.py
@@ -14,33 +14,20 @@ def get_new_brick(color, position):
 class Bricks(Turtle):
     def __init__(self):
         super().__init__()
-        self.__bricks_array = []
-        self.__initialize()
+        self.bricks_array = []
+        self.initialize()
 
-    def __initialize(self):
-        self.__initial_number = 13 * 4
+    def initialize(self):
         for i in range(0, 13):
             new_brick = get_new_brick('yellow', (267 + -45 * i, 100))
-            self.__bricks_array.append(new_brick)
+            self.bricks_array.append(new_brick)
             new_brick = get_new_brick('green', (267 + -45 * i, 125))
-            self.__bricks_array.append(new_brick)
+            self.bricks_array.append(new_brick)
             new_brick = get_new_brick('orange', (267 + -45 * i, 150))
-            self.__bricks_array.append(new_brick)
+            self.bricks_array.append(new_brick)
             new_brick = get_new_brick('red', (267 + -45 * i, 175))
-            self.__bricks_array.append(new_brick)
+            self.bricks_array.append(new_brick)
 
     def remove(self, brick):
         brick.reset()
-        self.__bricks_array.remove(brick)
-
-    def get_initial_number(self):
-        return self.__initial_number
-
-    def get_broken_number(self):
-        return self.get_initial_number() - self.get_current_number()
-
-    def get_current_number(self):
-        return len(self.__bricks_array)
-
-    def get_bricks(self):
-        return self.__bricks_array
+        self.bricks_array.remove(brick)
--- main.py
+++ main.py
@@ -3,61 +3,48 @@ from turtle import Screen
 from bricks import Bricks
 from paddle import Paddle
 from ball import Ball
-from score_board import Scoreboard
 import time
 
-
-def is_game_over() -> bool:
-    if ball.ycor() <= -280:
-        return True
-    return False
-
-
-def collision_with_edges(ball):
-    if ball.xcor() >= 290 or ball.xcor() <= -290:
-        ball.change_x_direction()
-    if ball.ycor() >= 290:
-        ball.change_y_direction()
-
-
-def collision_with_bricks(ball, bricks):
-    for brick in bricks.get_bricks():
-        if ball.distance(brick) <= 25:
-            bricks.remove(brick)
-
-
-def collision_with_paddle(ball, paddle):
-    if ball.ycor() - 10 <= paddle.ycor() and ball.distance(paddle) <= 45:
-        ball.change_y_direction()
-
-
 screen = Screen()
-
 screen.setup(width=600, height=600)
 screen.title('Breakout Game')
 screen.bgcolor('black')
 screen.tracer(0)
 
-paddle = Paddle(position_x=0, position_y=-280)
-ball = Ball(position_x=0, position_y=-250)
+paddle = Paddle()
+ball = Ball()
 bricks = Bricks()
-scoreboard = Scoreboard(position_x=220, position_y=270)
 
 screen.listen()
 screen.onkeypress(paddle.go_left, 'Left')
 screen.onkeypress(paddle.go_right, 'Right')
 
-while not is_game_over():
-    time.sleep(0.03)
+game_is_on = True
 
-    collision_with_edges(ball)
-    collision_with_bricks(ball, bricks)
-    collision_with_paddle(ball, paddle)
+while game_is_on:
+    time.sleep(0.05)
+
+    # edges collision
+    if ball.xcor() >= 290 or ball.xcor() <= -290:
+        ball.change_x_direction()
+    if ball.ycor() >= 290:
+        ball.change_y_direction()
+    elif ball.ycor() <= -280:
+        game_is_on = False
+        break
+
+    # bricks collision
+
+    for brick in bricks.bricks_array:
+        if ball.distance(brick) <= 25:
+            bricks.remove(brick)
+
+    # paddle collision
+
+    if ball.ycor() - 10 <= paddle.ycor() and ball.distance(paddle) <= 45:
+        ball.change_y_direction()
 
     ball.move()
     screen.update()
-    scoreboard.update_score(
-        bricks.get_initial_number() - bricks.get_current_number())
 
-scoreboard.game_over()
 screen.exitonclick()
