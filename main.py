e77d19dd3fb3a63ecadf94a42035adae2595ac9d
Milica98 <55503356+Milica98@users.noreply.github.com>
2024-01-20 21:57:11 +0100

    clean code

--- main.py
+++ main.py
@@ -5,6 +5,31 @@ from paddle import Paddle
 from ball import Ball
 import time
 
+
+def is_game_over() -> bool:
+    if ball.ycor() <= -280:
+        return True
+    return False
+
+
+def collision_with_edges(ball):
+    if ball.xcor() >= 290 or ball.xcor() <= -290:
+        ball.change_x_direction()
+    if ball.ycor() >= 290:
+        ball.change_y_direction()
+
+
+def collision_with_bricks(ball, bricks):
+    for brick in bricks.bricks_array:
+        if ball.distance(brick) <= 25:
+            bricks.remove(brick)
+
+
+def collision_with_paddle(ball, paddle):
+    if ball.ycor() - 10 <= paddle.ycor() and ball.distance(paddle) <= 45:
+        ball.change_y_direction()
+
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title('Breakout Game')
@@ -19,30 +44,12 @@ screen.listen()
 screen.onkeypress(paddle.go_left, 'Left')
 screen.onkeypress(paddle.go_right, 'Right')
 
-game_is_on = True
-
-while game_is_on:
+while not is_game_over():
     time.sleep(0.05)
 
-    # edges collision
-    if ball.xcor() >= 290 or ball.xcor() <= -290:
-        ball.change_x_direction()
-    if ball.ycor() >= 290:
-        ball.change_y_direction()
-    elif ball.ycor() <= -280:
-        game_is_on = False
-        break
-
-    # bricks collision
-
-    for brick in bricks.bricks_array:
-        if ball.distance(brick) <= 25:
-            bricks.remove(brick)
-
-    # paddle collision
-
-    if ball.ycor() - 10 <= paddle.ycor() and ball.distance(paddle) <= 45:
-        ball.change_y_direction()
+    collision_with_edges(ball)
+    collision_with_bricks(ball, bricks)
+    collision_with_paddle(ball, paddle)
 
     ball.move()
     screen.update()
